f6e1cd1a4fef9e8b4e86b127f5d686225b4b3db5
Paxton <paxton450@gmail.com>
2021-09-06 17:55:28 -0400

    Working on typeform images to google drive script

--- gdrive/GoogleDriveOperations.py
+++ gdrive/GoogleDriveOperations.py
@@ -1,37 +1,62 @@
 from __future__ import print_function
+
+import logging
 import os.path
+import pickle
+import sys
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from googleapiclient.http import MediaFileUpload
 import mimetypes
 
 # If modifying these scopes, delete the file token.json to make a new one.
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-class GDrive():
-    
-    def __init__(self):
-        creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
 
-        self.service = build('drive', 'v3', credentials=creds)
+class GDrive():
+
+    def __init__(self):
+        """
+        The constructor for GoogleAPI class
+
+        :param kwargs: used for continuing a log file; specified by logFile - string
+        """
+        try:
+            creds = None
+
+            # The file token.pickle stores the user's access and refresh tokens and is
+            # created automatically when the authorization flow completes for the first time
+            os.chdir(os.path.dirname(sys.argv[0]))
+            print(sys.argv[0])
+
+            if os.path.exists('token.pickle'):
+                with open('token.pickle', 'rb') as token:
+                    creds = pickle.load(token)
+
+            # If there are no (valid) credentials available, let the user log in.
+            if not creds or not creds.valid:
+
+                if creds and creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                    creds = flow.run_local_server(port=0)
+
+                # Save the credentials for the next run
+                with open('token.pickle', 'wb') as token:
+                    pickle.dump(creds, token)
+
+        except Exception as e:
+            logging.warning('Google Drive/Sheets API could not be initialized.')
+            logging.error(
+                'ERROR INITIALIZING GOOGLE SERVICES. ' + str(e.args[1] if len(e.args) > 1 else e.args[0]).upper())
+            raise e
+
+        else:
+            self.service = build('drive', 'v3', credentials=creds)
 
     def createFolder(self, folderName, **kwargs):
         # Function to create new folder 
@@ -45,21 +70,21 @@ class GDrive():
 
         # Default is top-level folder (if no parent folder info is given)
         file_metadata = {
-                'name': folderName,
-                'mimeType': 'application/vnd.google-apps.folder'
-            }
-        
+            'name': folderName,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+
         # If parent folder's ID is given, new folder will be created as subfolder under parent folder
         if kwargs.get('parentID'):
             file_metadata['parents'] = [kwargs.get('parentID')]
-        
+
         # if name of folder given instead of ID, search for folder in Drive 
         # (will put under first folder found with matching name)
-        elif kwargs.get('parentPath'): 
+        elif kwargs.get('parentPath'):
             file_metadata['parents'] = [self.folderIDFromPath(kwargs.get('parentPath'))]
 
         file = self.service.files().create(body=file_metadata, fields='id').execute()
-        
+
         folder_id = file.get('id')
         print('Folder ID: %s' % folder_id)
         return folder_id
@@ -81,7 +106,7 @@ class GDrive():
 
         # if name of folder given instead of ID, search for folder in Drive 
         # (will put under first folder found with matching name)
-        elif kwargs.get('parentPath'): 
+        elif kwargs.get('parentPath'):
             file_metadata['parents'] = [self.folderIDFromPath(kwargs.get('parentPath'))]
 
         fileType = mimetypes.guess_type(sourceFilePath)[0]
@@ -90,7 +115,7 @@ class GDrive():
 
         media = MediaFileUpload(sourceFilePath, mimetype=fileType)
         file = self.service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-        
+
         print('File ID: %s' % file.get('id'))
         return file.get('id')
 
@@ -104,19 +129,20 @@ class GDrive():
         folder_id = 'root'
 
         for item in path_items:
-        
+
             response = self.service.files().list(
-                q="mimeType='application/vnd.google-apps.folder' and name='{}' and '{}' in parents".format(item, folder_id),
+                q="mimeType='application/vnd.google-apps.folder' and name='{}' and '{}' in parents".format(item,
+                                                                                                           folder_id),
                 spaces='drive',
                 fields='files(id, name)'
             ).execute()
 
             if len(response.get('files', [])) == 0:
                 raise Exception('Folder not found')
-            
+
             file = response.get('files', [])[0]
             print('Found folder/file: %s (%s)' % (file.get('name'), file.get('id')))
-            folder_id = file.get('id')            
+            folder_id = file.get('id')
 
         return folder_id
 
@@ -134,4 +160,3 @@ class GDrive():
         for item in response.get('files', []):
             files[item.get('name')] = item.get('id')
         return files
-
